b4c168e3756adf3c8512fa22e542e38a695ce985
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-08-12 18:52:10 -0300

    status bar

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, SafeAreaView } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 import Main from './src/main.js'
 
 export default function App() {
   return (
     <SafeAreaView style={style.container}>
+      <StatusBar barStyle='light-content' backgroundColor='#8B10AE' />
       <Main />
     </SafeAreaView>
   );
