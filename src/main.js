99c5f03b111cf7b02fceeb0a050f29787a7ff6e6
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-08-12 19:56:38 -0300

    creating and styling Card component

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
 import React from 'react'
-import { Container } from './styles'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import Header from './components/Header'
 import Tabs from './components/Tabs'
+
+import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles'
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name='attach-money' size={28} color='#666' />
+            <Icon name='visibility-off' size={28} color='#666' />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 365.322,67</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferencia de R$ 20, 00 recebida de Alguém hoje às 08:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   )
